ea562ea0499693737acbefe0e28a0a898314195d
antonio <ant.sbe@protonmail.com>
2025-06-24 19:27:29 +0200

    add: test spectral accel and plots

--- SDOF_model.py
+++ SDOF_model.py
@@ -6,6 +6,7 @@
 import openseespy.opensees as ops
 # import opsvis as opsv
 import matplotlib.pyplot as plt
+import plot_settings as plt_set
 import numpy as np
 import pickle
 
@@ -110,7 +111,8 @@ ops.rayleigh(alphas, 0, 0, 0)
 
 # Parameters for the analysis
 dt = 0.005
-DtAnalysis = dt/2
+step_dt = 1
+DtAnalysis = dt/step_dt
 TMaxAnalysis = len(acc_data) * dt
 n_steps = int(TMaxAnalysis/DtAnalysis)+1
 dof_analysis = 1
@@ -133,7 +135,7 @@ top_node, basenodes = 11, [1]
 
 tCurrent = ops.getTime()
 time = []
-D,R = [], []
+D, R, acc = [], [], []
 ok, step = 0, 0
 
 while ok == 0 and tCurrent < TMaxAnalysis:
@@ -155,46 +157,71 @@ while ok == 0 and tCurrent < TMaxAnalysis:
 
     D.append(ops.nodeDisp(top_node,dof_analysis))
     R.append(-sum([ops.nodeReaction(node, dof_analysis)/1000 for node in basenodes]))
+    # take acceleration at node 11
+    acc.append(ops.nodeAccel(top_node, dof_analysis)/g)
     step += 1
 
 # Maximum displacement and index
 d_max = max(D, key=abs)
 id_max = D.index(d_max)
+S_d = acc_data[int(id_max/step_dt)] / g *a_g
+S_d_2 = ((2*np.pi/T_1)**2 * d_max) / g
 
-print(f"Maximum displacement: {d_max:.2f} mm at time {time[id_max]:.2f} s, Reaction: {R[id_max]:.2f} kN")
-
+# print(id_max, S_d, S_d_2)
 
 # Some plots
-fig = plt.figure()
+fig = plt.figure(figsize=plt_set.fig_size)
 ax = fig.gca()
 ax.plot(D,R, c = "midnightblue")
 ax.scatter(d_max, R[id_max], color='red', label=f'Max: {d_max:.2f} mm at {time[id_max]:.2f} s')
 ax.grid(alpha = 0.25)
-ax.set_xlabel("Displacement [mm]")
-ax.set_ylabel("Force [kN]")
+ax.set_xlabel("Displacement [mm]"), ax.set_ylabel("Force [kN]")
 ax.axhline(0, color='grey', linewidth = 1, alpha=0.6), ax.axvline(0, color='grey', linewidth = 1, alpha=0.6)
-plt.savefig('./figs/displacement_force.png', dpi=150, bbox_inches='tight')
+plt.tight_layout(), plt.savefig('./figs/displacement_force.png', dpi=150, bbox_inches='tight')
 
 
 
-fig = plt.figure()
+fig = plt.figure(figsize=plt_set.fig_size)
 ax = fig.gca()
 ax.plot(time, D, c = "midnightblue")
 ax.scatter(time[id_max], d_max, color='red', label=f'Max: {d_max:.2f} mm at {time[id_max]:.2f} s')
 ax.grid(alpha = 0.25)
-ax.set_ylabel("Displacement [mm]")
-ax.set_xlabel("Time [s]")
+ax.set_ylabel("Displacement [mm]"), ax.set_xlabel("Time [s]")
+ax.set_xlim(0)
 ax.axhline(0, color='grey', linewidth = 1, alpha=0.6), ax.axvline(0, color='grey', linewidth = 1, alpha=0.6)
-plt.savefig('./figs/displacement_time.png', dpi=150, bbox_inches='tight')
+plt.tight_layout(), plt.savefig('./figs/displacement_time.png', dpi=150, bbox_inches='tight')
 
-fig = plt.figure()
+
+fig = plt.figure(figsize=plt_set.fig_size)
 ax = fig.gca()
-ax.plot(np.linspace(0, TMaxAnalysis, len(acc_data)), acc_data, c = "midnightblue")
+ax.plot(np.linspace(0, TMaxAnalysis, len(acc_data)), (acc_data/g)*a_g, c = "midnightblue")
+ax.scatter(np.linspace(0, TMaxAnalysis, len(acc_data))[int(id_max/step_dt)], S_d, color='red', label=f'Spectral: {S_d:.2f} g')
+# ax.plot(time, acc, c = "firebrick", label = "Top node")
 ax.grid(alpha = 0.25)
-ax.set_ylabel("Acceleration [m/s²]")
-ax.set_xlabel("Time [s]")
+ax.set_ylabel("Acceleration [g]"), ax.set_xlabel("Time [s]")
+ax.set_xlim(0)
+ax.legend()
 ax.axhline(0, color='grey', linewidth = 1, alpha=0.6), ax.axvline(0, color='grey', linewidth = 1, alpha=0.6)
-plt.savefig('./figs/acceleration_time.png', dpi=150, bbox_inches='tight')
+plt.tight_layout(), plt.savefig('./figs/acceleration_time.png', dpi=150, bbox_inches='tight')
 
 
-# plt.show()
+
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(7, 4.5), sharex=True)
+ax1.plot(time, D, c = "midnightblue")
+ax1.scatter(time[id_max], d_max, facecolors='none', edgecolors='firebrick', label=rf'Max $\delta$: {d_max:.2f} mm at {time[id_max]:.2f} s')
+ax1.grid(alpha = 0.25)
+ax1.set_ylabel("Displacement [mm]")
+ax1.axvline(time[id_max], color='dimgrey', linewidth = 1, alpha= 0.8, linestyle = "--")
+ax1.axhline(0, color='grey', linewidth = 1, alpha=0.6), ax1.axvline(0, color='grey', linewidth = 1, alpha= 0.6) 
+ax1.set_xlim(0)
+ax1.legend()
+
+ax2.plot(np.linspace(0, TMaxAnalysis, len(acc_data)), (acc_data/g)*a_g, c = "midnightblue")
+ax2.scatter(np.linspace(0, TMaxAnalysis, len(acc_data))[int(id_max/step_dt)], S_d, facecolors='none', edgecolors='firebrick', label=rf'$a_g$: {S_d:.4f} g')
+ax2.grid(alpha = 0.25)
+ax2.set_ylabel("Acceleration [g]"), ax2.set_xlabel("Time [s]")
+ax2.axvline(np.linspace(0, TMaxAnalysis, len(acc_data))[int(id_max/step_dt)], color='dimgrey', linewidth = 1, alpha= 0.8, linestyle = "--")
+ax2.axhline(0, color='grey', linewidth = 1, alpha=0.6), ax2.axvline(0, color='grey', linewidth = 1, alpha= 0.6)
+ax2.legend()
+ax1.set_title(rf"Accel {id_acc} / $a_g$ {a_g:.2f} g / $T_1$ = {T_1:.2f} s")
+plt.tight_layout(), plt.savefig('./figs/displacement_acceleration_time.png', dpi=150, bbox_inches='tight')
